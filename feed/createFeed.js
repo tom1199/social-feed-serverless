a723d44de602e2b0dbac3505347c21c61e68bb05
tom1199 <ec2-user@ip-172-31-41-60.ap-southeast-1.compute.internal>
2018-05-20 11:42:01 +0000

    setup dynamodb

--- feed/createFeed.js
+++ feed/createFeed.js
@@ -1,26 +1,96 @@
 'use strict';
 
-exports.handler = function(event, context, callback) {
+const dynamodb = require("../dynamodb");
+const uuid = require("uuid");
 
-    const newFeed = {
-        "title": "my new feed",
-        "image_url": "http://www.google.com"
-    };
+function isValidateFeed(feed) {
+    if (feed.imageUrl === undefined) {
+        return false;
+    }
+    return true;
+}
+
+exports.handler = function(event, context, callback) {
+    console.log("body = ", event.body);
+    const data = JSON.parse(event.body || "{}");
+    const timestamp = new Date().getTime();
     
-    const body = {
-        message: "success",
-        feed: newFeed
-    };
+    if (isValidateFeed(data) === false) {
+        console.error('invalid data');
+        const body = {
+            error: "invalid data",
+            message: "invalid feed input" 
+        };
+            
+        const response = {
+            statusCode: 400,
+            headers: { 
+                'Content-Type': 'application/json', 
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(body),
+            isBase64Encoded: false
+        }
+            
+        callback(null, response);
+        return;
+    }
     
-    const response = {
-        statusCode: 201,
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
+    const params = {
+        TableName: process.env.FEED_TABLE,
+        Item: {
+            id: uuid.v1(),
+            title: data.title,
+            imageUrl: data.imageUrl,
+            createdAt: timestamp,
+            updatedAt: timestamp,
         },
-        isBase64Encoded: false
     };
+
+    // write the todo to the database
+    dynamodb.put(params, (error, data) => {
+        
+        // handle potential errors
+        if (error) {
+            console.error(error);
+            
+            const body = {
+                error: "Couldn\'t create new feed.",
+                message: "Couldn\'t create new feed." 
+            };
+            
+            const response = {
+                statusCode: error.statusCode || 501,
+                headers: { 
+                    'Content-Type': 'application/json', 
+                    'Access-Control-Allow-Origin': '*'
+                },
+                body: JSON.stringify(body),
+                isBase64Encoded: false
+            }
+            
+            callback(null, response);
+            
+            return;
+        }
+        
+        const body = {
+            message: "success",
+            feed: data
+        };
     
-    callback(null, response);
+        const response = {
+            statusCode: 201,
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            isBase64Encoded: false
+        };
+    
+        callback(null, response);
+    
+    });
+ 
 };
