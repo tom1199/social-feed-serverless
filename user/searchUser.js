c64f4cc3e48bac6bdabefed2275b068738888e26
Win Htut Aung <winhtutaungster@gmail.com>
2018-05-20 23:59:21 +0800

    Get All User

---
+++ user/searchUser.js
@@ -0,0 +1,47 @@
+const randomBytes = require('crypto').randomBytes;
+
+const AWS = require('aws-sdk');
+
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = (event, context, callback) => {
+    if (!event.requestContext.authorizer) {
+        errorResponse('Authorization not configured', context.awsRequestId, callback);
+        return;
+    }
+    const requestBody = JSON.parse(evnet.body);
+
+    var users = searchUser(requestBody);
+
+    const response = {
+        statusCode: 200,
+        body: users,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        isBase64Encoded: false
+    }
+   
+    callback(null, response);
+}
+
+function searchUser(requestBody) {
+    const users = [
+        {
+            "userId": 1,
+            "name": "Win",
+            "email": "win@gmail.com",
+            "follow": true,
+            "image_url": "www.google.com"
+        },
+        {
+            "userId": 2,
+            "name": "Treza",
+            "email": "treza@gmail.com",
+            "follow": true,
+            "image_url": "www.google.com"
+        },
+    ];
+    return users;
+}
