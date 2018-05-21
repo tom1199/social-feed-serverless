dc4bb277bc6eb47d91e2a4a03518c442034994da
Win Htut Aung <winhtutaungster@gmail.com>
2018-05-22 01:44:05 +0800

    Search User with Dynamo

--- user/createUser.js
+++ user/createUser.js
@@ -11,5 +11,86 @@ exports.handler = (event, context, callback) => {
     console.log("body = ", event.body);
     const data = JSON.parse(event.body || "{}");
     const timestamp = new Date().getTime();
+
+    if (isValidUser(data) === false) {
+        console.error('invalid data');
+        const body = {
+            error: "Bad Request",
+            message: "invalid user input" 
+        };
+            
+        const response = {
+            statusCode: 400,
+            headers: { 
+                'Content-Type': 'application/json', 
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(body),
+            isBase64Encoded: false
+        }
+            
+        callback(null, response);
+        return;
+    }
+
+    const newUser = {
+        id: uuid.v1(),
+        userName: data.title,
+        imageUrl: data.imageUrl,
+        email: data.ownerId,
+        createdAt: timestamp,
+        updatedAt: timestamp,
+    };
+
+    const params = {
+        TableName: process.env.USER_TABLE,
+        Item: newUser,
+    };
+
+     // write the todo to the database
+     dynamodb.put(params, (error) => {
+        
+        // handle potential errors
+        if (error) {
+            console.error(error);
+            
+            const body = {
+                error: "Couldn\'t create new user.",
+                message: "Couldn\'t create new user." 
+            };
+            
+            const response = {
+                statusCode: error.statusCode || 501,
+                headers: { 
+                    'Content-Type': 'application/json', 
+                    'Access-Control-Allow-Origin': '*'
+                },
+                body: JSON.stringify(params),
+                isBase64Encoded: false
+            }
+            
+            callback(null, response);
+            
+            return;
+        }
+        
+        const body = {
+            message: "success",
+            feed: newUser
+        };
+    
+        const response = {
+            statusCode: 201,
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            isBase64Encoded: false
+        };
+    
+        callback(null, response);
+    
+    });
 };
 
