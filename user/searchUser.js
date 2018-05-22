9480f70cc82929743ef2a4c6789d27d61d072f66
Win Htut Aung <winhtutaungster@gmail.com>
2018-05-23 00:52:24 +0800

    Search User Connect with DynamoDB

--- user/searchUser.js
+++ user/searchUser.js
@@ -12,26 +12,53 @@ exports.handler = (event, context, callback) => {
     // }
     const requestBody = JSON.parse(event.body || "{}");
 
-    var users = searchUser(requestBody);
+    // var users = searchUser(requestBody);
 
     
-    const body = {
-        message: "success",
-        users: users
+    const params = {
+        TableName: process.env.USER_TABLE
     };
-    
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        },
-        isBase64Encoded: false
-    };
-   
-    callback(null, response);
+    dynamodb.scan(params, (error, result) => {
+        if (error) {
+            console.error(error);
+            
+            const body = {
+                error: "Internal Server Error" + error,
+                message: "Couldn\'t get feeds." 
+            };
+            
+            const response = {
+                statusCode: error.statusCode || 501,
+                headers: { 
+                    'Content-Type': 'application/json', 
+                    'Access-Control-Allow-Origin': '*'
+                },
+                body: JSON.stringify(body),
+                isBase64Encoded: false
+            }
+            
+            callback(null, response);
+            return;
+        }
+
+
+        const body = {
+            message: "success",
+            users: result.Items
+        };
+        
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            isBase64Encoded: false
+        };
+        callback(null, response);
+    });
 };
 
 
