9b30b7739a395b1bd9ae6f9d9250f88080d4e0e4
Win Htut Aung <winhtutaungster@gmail.com>
2018-05-31 21:10:49 +0800

    Unfollow User

--- user/unfollowUser.js
+++ user/unfollowUser.js
@@ -7,8 +7,8 @@ const Promise = require("promise");
 
 function isValidateRequest(event) {
     const body = JSON.parse(event.body || "{}")
-    const userId = event.pathParameters.userId;
-    const followedUserId = body.followedUserId;
+    const userId = event.pathParameters.userId + '';
+    const followedUserId = body.followedUserId + '';
     if (userId === undefined) {
         console.log("Missing User Id");
         return false;
@@ -27,9 +27,9 @@ exports.handler = function(event, context, callback) {
         return;
     }
     
-    const userId = event.pathParameters.userId;
+    const userId = event.pathParameters.userId + '';
     const body = JSON.parse(event.body || "{}");
-    const followedUserId = body.followedUserId;
+    const followedUserId = body.followedUserId + '';
     const timestamp = new Date().getTime();
     
     function updateUserFollowTable() {
